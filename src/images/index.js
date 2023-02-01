1dda8d9ce8ad676399292399799af957fd755aba
nomansd1 <noman.sd114@gmail.com>
2023-01-31 19:20:30 -0800

    working on global components for homepage design

---
+++ src/images/index.js
@@ -0,0 +1,22 @@
+export { default as dropdownCarret } from '../images/dropdowncarret.png';
+export { default as bottomHero } from '../images/bottomhero.png';
+export { default as logo } from '../images/brandlogo.png';
+export { default as company1 } from '../images/company1.png';
+export { default as company2 } from '../images/company2.png';
+export { default as gridTile1 } from '../images/gridtile1.png';
+export { default as hero } from '../images/hero.png';
+export { default as lightMode } from '../images/lightmode.png';
+export { default as ola } from '../images/orangeleftarrow.png';
+export { default as partnerGrid } from '../images/partnergrid.png';
+export { default as port1 } from '../images/port1.png';
+export { default as port2 } from '../images/port2.png';
+export { default as port3 } from '../images/port3.png';
+export { default as port4 } from '../images/port4.png';
+export { default as port5 } from '../images/port5.png';
+export { default as port6 } from '../images/port6.png';
+export { default as  testimo } from '../images/testimo.png';
+export { default as  wla } from '../images/whiteleftarrow.png';
+export { default as  wra } from '../images/whiterightarrow.png';
+
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  purge:['./src/**/*.{js,jsx,ts,tsx}'],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
