a70bb305b66c2a542a30ee4071a48ec3494af579
nomansd1 <noman.sd114@gmail.com>
2023-02-04 04:06:08 -0800

    add a why us component in services page

--- src/components/services/WhyUs.js
+++ src/components/services/WhyUs.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { whyTile } from '../../images';
 
+
 function WhyUs() {
     const [darkmodeToggler, setDarkmodeToggler] = useState(false);
   return (
