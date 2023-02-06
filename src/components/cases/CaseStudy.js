e82dd6bf927fc02a8f66026fc9e558d030e95b7f
nomansd1 <noman.sd114@gmail.com>
2023-02-06 02:02:48 -0800

    add some more pages

---
+++ src/components/cases/CaseStudy.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+function CaseStudy() {
+    return (
+        <div>
+            <div className='dark:bg-[#281717] mt-16 bg-green-800 px-16 py-16'>
+                <h1 className='text-4xl dark:text-white font-bold '>Case Study's</h1>
+                <hr className="mt-10 border-[#f35544]" />
+                <div className='mt-6'>
+                    <ul class="flex flex-wrap text-sm font-medium text-center text-gray-500 dark:text-gray-400">
+                        <li class="mr-2">
+                            <a href="#" class="inline-block px-4 py-2.5 text-[#f35544] hover:text-white bg-transparent hover:bg-[#f35544] rounded-[35px] border-2 border-[#f35544] active:bg-[#f35544] focus:bg-[#f35544] focus:text-white" aria-current="page">AI Development</a>
+                        </li>
+                        <li class="mr-2">
+                            <a href="#" class="inline-block px-4 py-2.5 text-[#f35544] hover:text-white bg-transparent hover:bg-[#f35544] rounded-[35px] border-2 border-[#f35544] active:bg-[#f35544] focus:bg-[#f35544] focus:text-white" aria-current="page">AI Development</a>
+                        </li>
+                        <li class="mr-2">
+                            <a href="#" class="inline-block px-4 py-2.5 text-[#f35544] hover:text-white bg-transparent hover:bg-[#f35544] rounded-[35px] border-2 border-[#f35544] active:bg-[#f35544] focus:bg-[#f35544] focus:text-white" aria-current="page">AI Development</a>
+                        </li>
+                        <li class="mr-2">
+                            <a href="#" class="inline-block px-4 py-2.5 text-[#f35544] hover:text-white bg-transparent hover:bg-[#f35544] rounded-[35px] border-2 border-[#f35544] active:bg-[#f35544] focus:bg-[#f35544] focus:text-white" aria-current="page">AI Development</a>
+                        </li>
+                        <li class="mr-2">
+                            <a href="#" class="inline-block px-4 py-2.5 text-[#f35544] hover:text-white bg-transparent hover:bg-[#f35544] rounded-[35px] border-2 border-[#f35544] active:bg-[#f35544] focus:bg-[#f35544] focus:text-white" aria-current="page">AI Development</a>
+                        </li>
+                        <li class="mr-2">
+                            <a href="#" class="inline-block px-4 py-2.5 text-[#f35544] hover:text-white bg-transparent hover:bg-[#f35544] rounded-[35px] border-2 border-[#f35544] active:bg-[#f35544] focus:bg-[#f35544] focus:text-white" aria-current="page">AI Development</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CaseStudy
---
+++ src/pages/casestudypage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import CaseStudy from '../components/cases/CaseStudy'
+
+function casestudypage() {
+  return (
+    <div>
+      <Header/>
+      <CaseStudy/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default casestudypage
